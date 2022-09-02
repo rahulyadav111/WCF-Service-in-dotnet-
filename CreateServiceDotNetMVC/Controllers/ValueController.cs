cc11347555046bae203303d12dc4832daa960400
Rahul Yadav <72229574+rahulyadav111@users.noreply.github.com>
2022-09-02 17:44:17 +0530

    Add files via upload

---
+++ CreateServiceDotNetMVC/Controllers/ValueController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CreateServiceDotNetMVC.Controllers
+{
+    public class ValueController : ApiController
+    {
+        // GET: Value
+       
+    }
+}
---
+++ CreateServiceDotNetMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CreateServiceDotNetMVC.MvcApplication" Language="C#" %>
